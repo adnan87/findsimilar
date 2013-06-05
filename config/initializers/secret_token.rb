bf057c4e616b2a4f2936190bfe2433b376ac0822
Sabarinath Iyer <sabarinathiyer@Sabarinath-Iyers-MacBook-Pro.local>
2013-06-05 18:13:21 +0530

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findsimilar::Application.config.secret_token = '22aa381eee1beb9bcccfe3ec882000790c7bd001b322d72ba10503abad176194e65e76378e1f338230c63a06ebda99e160fecfcce1ca8da423c455fc04bae52d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Findsimilar::Application.config.session_store :cookie_store, key: '_findsimilar_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Findsimilar::Application.config.session_store :active_record_store
